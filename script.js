31efb5ccb3f829e2a50b3a51f06b4ba0f3dddf10
heygenovez <47248489+heygenovez@users.noreply.github.com>
2020-02-12 18:16:17 -0300

    Página de login e inscrição!
    Página de login e inscrição animada com HTML, CSS e JAVASCRIPT

---
+++ script.js
@@ -0,0 +1,6 @@
+function togglePage() {
+      document.querySelector(".overlay-signin").classList.toggle("show");
+      document.querySelector(".overlay-signup").classList.toggle("hide");
+      document.querySelector(".signup").classList.toggle("show");
+      document.querySelector(".signin").classList.toggle("hide");
+    }
